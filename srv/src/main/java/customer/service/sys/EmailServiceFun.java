ce59c92c1b771f4089b7ae6776d39fabdeb7a66f
dd-li <2644715037@qq.com>
2024-08-19 13:31:58 +0800

    sc

--- srv/src/main/java/customer/service/sys/EmailServiceFun.java
+++ srv/src/main/java/customer/service/sys/EmailServiceFun.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class EmailServiceFun {
-    @Autowired
-    private JavaMailSender emailSender;
+    // @Autowired
+    // private JavaMailSender emailSender;
 }
