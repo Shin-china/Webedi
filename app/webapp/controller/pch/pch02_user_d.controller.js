d5c1aea002b5afdd918dead646787e3d5bfaff69
xy-wang <xiaoyue.wang@sh.shin-china.com>
2024-08-19 16:36:27 +0800

    heb

--- app/webapp/controller/pch/pch02_user_d.controller.js
+++ app/webapp/controller/pch/pch02_user_d.controller.js
@@ -1,32 +1,40 @@
 sap.ui.define([
-	"umc/app/controller/BaseController",
-	"sap/ui/model/odata/v2/ODataModel",
-	"sap/m/MessageBox"
-], function(
-	Controller,
-	ODataModel,
-	MessageBox
-) {
-	"use strict";
-
-	return Controller.extend("umc.app.controller.pch.pch02_user_d", {
-		// onInit: function() {
-		// 	// 设置默认 OData 模型
-		// 	// this._setCreateAble(false);
-		// 	this._setDefaultDataModel("TableService");
-
-		// 	// 清除消息
-		// 	this.MessageTools._clearMessage();
-		// },
-
-		// onSearch :function(oEvent) {
-		// 	console.log('11111')
-		// },
-
-		// onRebind:function(oEvent) {
-		// 	console.log('11111')
-		// },
-		
-
-	});
+    "umc/app/Controller/BaseController",
+    "sap/ui/model/Filter",
+    "umc/app/model/formatter",
+    "sap/ui/export/Spreadsheet"
+], function (Controller, Filter, formatter, Spreadsheet) {
+    "use strict";
+    return Controller.extend("umc.app.controller.pch.pch02_user_d", {
+        formatter: formatter,
+        onInit: function () {
+            this._LocalData = this.getOwnerComponent().getModel("local");
+            this._oDataModel = this.getOwnerComponent().getModel();
+            this._ResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+            this._oDataModel.attachBatchRequestCompleted(function(oEvent){
+                this.setBusy(false);
+                var errors = this._LocalData.getProperty("/errors");
+                if (errors) {
+                    // 处理错误
+                }
+                this._LocalData.setProperty("/errors", "");
+            }.bind(this));
+        },
+        setBusy: function(busy){
+            this._LocalData.setProperty("/busy", busy);
+        },
+        // 
+        onBeforeExport: function(oEvent) {
+            var oSettings = oEvent.getParameter("exportSettings");
+            if (oSettings) {
+                console.log("onBeforeExport called");
+                console.log("Export Settings:", oSettings);
+                var oDate = new Date();
+                var sDate = oDate.toISOString().slice(0, 10).replace(/-/g, '');
+                var sTime = oDate.toTimeString().slice(0, 8).replace(/:/g, '');
+                // 设置文件名为当前日期和时间
+                oSettings.fileName = `仕入先コード_納期回答照会_${sDate}_${sTime}.xlsx`;
+            }
+        }
+    });
 });
