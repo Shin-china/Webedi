c42bb1727f77c950d4c07d9b2dc41e1d14b575ac
xy-wang <xiaoyue.wang@sh.shin-china.com>
2024-08-22 13:55:18 +0800

    wxy

--- app/webapp/controller/pch/pch02_user_d.controller.js
+++ app/webapp/controller/pch/pch02_user_d.controller.js
@@ -7,11 +7,13 @@ sap.ui.define([
     "use strict";
     return Controller.extend("umc.app.controller.pch.pch02_user_d", {
         formatter: formatter,
+
         onInit: function () {
             this._LocalData = this.getOwnerComponent().getModel("local");
             this._oDataModel = this.getOwnerComponent().getModel();
             this._ResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
-            this._oDataModel.attachBatchRequestCompleted(function(oEvent){
+
+            this._oDataModel.attachBatchRequestCompleted(function(oEvent) {
                 this.setBusy(false);
                 var errors = this._LocalData.getProperty("/errors");
                 if (errors) {
@@ -20,9 +22,11 @@ sap.ui.define([
                 this._LocalData.setProperty("/errors", "");
             }.bind(this));
         },
+
         setBusy: function(busy){
             this._LocalData.setProperty("/busy", busy);
         },
+
         onExport: function () {
             var oTable = this.getView().byId("detailTable");
             var aSelectedIndices = oTable.getSelectedIndices();
@@ -55,7 +59,17 @@ sap.ui.define([
                 sap.m.MessageToast.show(this._ResourceBundle.getText("exportFinished"));
             }.bind(this));
         },
+
         onBeforeExport: function(oEvent) {
+            var oTable = this.getView().byId("detailTable");
+            var aSelectedIndices = oTable.getSelectedIndices();
+
+            if (aSelectedIndices.length === 0) {
+                sap.m.MessageToast.show(this._ResourceBundle.getText("選択されたデータがありません、データを選択してください。")); // 提示未选择数据
+                oEvent.preventDefault(); // 取消导出操作
+                return;
+            }
+
             var oSettings = oEvent.getParameter("exportSettings");
             if (oSettings) {
                 console.log("onBeforeExport called");
@@ -66,6 +80,23 @@ sap.ui.define([
                 // 设置文件名为当前日期和时间
                 oSettings.fileName = `仕入先コード_納期回答照会_${sDate}_${sTime}.xlsx`;
             }
+        },
+
+        onResend: function () {
+            // 这里的 `this` 指向当前的 Controller 实例
+            this.getModel().callFunction("/PCH02_CONFIRMATION_REQUEST", {
+                method: "POST",
+                urlParameters: this.getData(),
+                // urlParameters: {
+                //     parms: "your_parameters_here" // 替换为实际参数
+                // },
+                success: function (result) {
+                    sap.m.MessageToast.show("Action executed successfully.");
+                },
+                error: function (oError) {
+                    sap.m.MessageToast.show("Error executing action.");
+                }
+            });
         }
     });
 });
