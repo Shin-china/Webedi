02254c886841e680b6cce1d0c2c9faec44a9b8da
Nero.w <zhao.wang@sh.shin-china.com>
2024-08-27 17:20:40 +0800

    20240827-pch01 commit

--- srv/src/main/java/customer/handlers/pch/Pch01Handler.java
+++ srv/src/main/java/customer/handlers/pch/Pch01Handler.java
@@ -9,7 +9,10 @@ import com.sap.cds.services.handler.annotations.ServiceName;
 import cds.gen.tableservice.PCH01CheckDATAContext;
 import cds.gen.tableservice.PCH01SaveDATAContext;
 import cds.gen.tableservice.TableService_;
+import customer.bean.pch.Pch01List;
+import customer.service.pch.Pch01Service;
 
+import com.alibaba.fastjson.JSON;
 import com.sap.cds.services.handler.EventHandler;
 
 @Component
@@ -18,11 +21,25 @@ public class Pch01Handler implements EventHandler {
 
     @Autowired
     ResourceBundleMessageSource rbms;
+
+    @Autowired
+    private Pch01Service Pch01Service;
+
   
     // check数据
     @On(event = "PCH01_CHECK_DATA")
     public void checkData(PCH01CheckDATAContext context) {
-
+    Pch01List list = JSON.parseObject(context.getShelfJson(), Pch01List.class);
+    Pch01Service.detailsCheck(list);
+    context.setResult(JSON.toJSONString(list));
     }
+
+      // 保存数据
+  @On(event = "PCH01_SAVE_DATA")
+    public void saveData(PCH01SaveDATAContext context) {
+    Pch01List list = JSON.parseObject(context.getShelfJson(), Pch01List.class);
+    Pch01Service.detailsSave(list);
+    context.setResult(JSON.toJSONString(list));
+  }
 
 }
