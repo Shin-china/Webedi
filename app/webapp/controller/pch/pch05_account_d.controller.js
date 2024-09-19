b9efd4fd9a38ed12f3cd9510bc7676a0cb146ff1
xy-wang <xiaoyue.wang@sh.shin-china.com>
2024-09-19 17:21:17 +0800

    wxypch05

--- app/webapp/controller/pch/pch05_account_d.controller.js
+++ app/webapp/controller/pch/pch05_account_d.controller.js
@@ -10,43 +10,88 @@ sap.ui.define([
 		formatter : formatter,
 
 		onInit: function () {
-		// 设置自己的 OData模型为默认模型
-		this._setDefaultDataModel("TableService");
-		//  设置版本号
-		this._setOnInitNo("PCH05", ".20240812.01");
-		// 设置选中框 无
-		// this._setSelectionNone("detailTable");
-		// this._setEditableAuth(true);
-		},
-	
-		// onPress: function (oEvent) {
-		// var oItem = oEvent.getSource();
-		// var oContext = oItem.getBindingContext();
-		// this._onPress(oEvent, "RouteEdit_sys01", oContext.getObject().ID);
-		// },
+            this._LocalData = this.getOwnerComponent().getModel("local");
+            this._oDataModel = this.getOwnerComponent().getModel();
+            this._ResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
 
-		onRebind: function (oEvent) {
-		// this._onListRebindDarft(oEvent);
-		this._onListRebindDarft(oEvent, true);
-		},
-	
-		onBeforeExport: function (oEvt) {
-		var mExcelSettings = oEvt.getParameter("exportSettings");
-		for (var i = 0; i < mExcelSettings.workbook.columns.length; i++) {
-			// this.CommTools._setExcelFormatDate(mExcelSettings, i, "VALID_DATE_FROM");
-			// this.CommTools._setExcelFormatDate(mExcelSettings, i, "VALID_DATE_TO");
-			// this.CommTools._setExcelFormatDateTime(mExcelSettings, i, "CD_TIME");
-			// this.CommTools._setExcelFormatDateTime(mExcelSettings, i, "UP_TIME");
-			// this.CommTools._setExcelFormatDateTime(mExcelSettings, i, "LAST_LOGIN_TIME");
-			}
-	
-		},
+            this._oDataModel.attachBatchRequestCompleted(function (oEvent) {
+                this.setBusy(false);
+                var errors = this._LocalData.getProperty("/errors");
+                if (errors) {
+                    // 处理错误
+                }
+                this._LocalData.setProperty("/errors", "");
+            }.bind(this));
+        },
+
+        setBusy: function (busy) {
+            this._LocalData.setProperty("/busy", busy);
+        },
+
+        onExport: function () {
+            var oTable = this.getView().byId("detailTable");
+            var aSelectedIndices = oTable.getSelectedIndices();
+            if (aSelectedIndices.length === 0) {
+                sap.m.MessageToast.show(this._ResourceBundle.getText("noSelection")); // 提示未选择数据
+                return;
+            }
+            var aSelectedData = aSelectedIndices.map(function (iIndex) {
+                return oTable.getContextByIndex(iIndex).getObject();
+            });
+            if (aSelectedData.length === 0) {
+                sap.m.MessageToast.show(this._ResourceBundle.getText("noSelection")); // 提示没有数据
+                return;
+            }
+            var aColumns = oTable.getColumns().map(function (oColumn) {
+                return {
+                    label: oColumn.getLabel().getText(),
+                    type: "string",
+                    property: oColumn.getTemplate().getBindingPath("text"),
+                    width: parseFloat(oColumn.getWidth())
+                };
+            });
+            new Spreadsheet({
+                dataSource: aSelectedData, // 仅传递选中的数据
+                columns: aColumns,
+                fileName: "Export.xlsx",
+                worker: false,  // Disable web worker for simplicity
+                beforeExport: this.onBeforeExport.bind(this)
+            }).build().finally(function () {
+                sap.m.MessageToast.show(this._ResourceBundle.getText("exportFinished"));
+            }.bind(this));
+        },
+
+        onBeforeExport: function (oEvent) {
+            var oTable = this.getView().byId("detailTable");
+            var aSelectedIndices = oTable.getSelectedIndices();
+
+            if (aSelectedIndices.length === 0) {
+                sap.m.MessageToast.show(this._ResourceBundle.getText("選択されたデータがありません、データを選択してください。")); // 提示未选择数据
+                oEvent.preventDefault(); // 取消导出操作
+                return;
+            }
+            // var mExcelSettings = oEvt.getParameter("exportSettings");
+            // for (var i = 0; i < mExcelSettings.workbook.columns.length; i++) {
+            //     // this.CommTools._setExcelFormatDate(mExcelSettings, i, "VALID_DATE_FROM");
+            //     // this.CommTools._setExcelFormatDate(mExcelSettings, i, "VALID_DATE_TO");
+            //     // this.CommTools._setExcelFormatDateTime(mExcelSettings, i, "CD_TIME");
+            //     // this.CommTools._setExcelFormatDateTime(mExcelSettings, i, "UP_TIME");
+            //     // this.CommTools._setExcelFormatDateTime(mExcelSettings, i, "LAST_LOGIN_TIME");
+            //     }
+          
+
+            var oSettings = oEvent.getParameter("exportSettings");
+            if (oSettings) {
+                console.log("onBeforeExport called");
+                console.log("Export Settings:", oSettings);
+                var oDate = new Date();
+                var sDate = oDate.toISOString().slice(0, 10).replace(/-/g, '');
+                var sTime = oDate.toTimeString().slice(0, 8).replace(/:/g, '');
+                // 设置文件名为当前日期和时间
+                oSettings.fileName = `買掛金明細_${sDate}${sTime}.xlsx`;
+            }
+        },
 		
-		// onCreate: function (oEvent) {
-		// var oItem = oEvent.getSource();
-		// var oContext = oItem.getBindingContext();
-		// this._onPress(oEvent, "RouteCre_sys01");
-		// },
 	
 	});
 });
--- app/webapp/controller/pch/pch05_account_l.controller.js
+++ app/webapp/controller/pch/pch05_account_l.controller.js
@@ -1,22 +1,97 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
-    "use strict";
+	"umc/app/Controller/BaseController",
+	"sap/ui/model/Filter",
+	"umc/app/model/formatter",
+	"sap/ui/export/Spreadsheet"
+], function (Controller, Filter, formatter,Spreadsheet) {
+	"use strict";	
 
     return Controller.extend("umc.app.controller.pch.pch05_account_l", {
-        onInit: function () {
-            // 在这里初始化列表视图的逻辑
-            // 例如：加载数据模型、设置默认值等
+		formatter : formatter,
+
+		onInit: function () {
+            this._LocalData = this.getOwnerComponent().getModel("local");
+            this._oDataModel = this.getOwnerComponent().getModel();
+            this._ResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+
+            this._oDataModel.attachBatchRequestCompleted(function (oEvent) {
+                this.setBusy(false);
+                var errors = this._LocalData.getProperty("/errors");
+                if (errors) {
+                    // 处理错误
+                }
+                this._LocalData.setProperty("/errors", "");
+            }.bind(this));
         },
 
-        onListItemPress: function (oEvent) {
-            // 当用户点击列表中的某项时的处理逻辑
-            const oItem = oEvent.getSource();
-            const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-            oRouter.navTo("RouteToDetail", {
-                // 假设你在这里需要传递一个参数
-                itemId: oItem.getBindingContext().getProperty("ID")
+        setBusy: function (busy) {
+            this._LocalData.setProperty("/busy", busy);
+        },
+
+        onExport: function () {
+            var oTable = this.getView().byId("detailTable");
+            var aSelectedIndices = oTable.getSelectedIndices();
+            if (aSelectedIndices.length === 0) {
+                sap.m.MessageToast.show(this._ResourceBundle.getText("noSelection")); // 提示未选择数据
+                return;
+            }
+            var aSelectedData = aSelectedIndices.map(function (iIndex) {
+                return oTable.getContextByIndex(iIndex).getObject();
             });
-        }
-    });
+            if (aSelectedData.length === 0) {
+                sap.m.MessageToast.show(this._ResourceBundle.getText("noSelection")); // 提示没有数据
+                return;
+            }
+            var aColumns = oTable.getColumns().map(function (oColumn) {
+                return {
+                    label: oColumn.getLabel().getText(),
+                    type: "string",
+                    property: oColumn.getTemplate().getBindingPath("text"),
+                    width: parseFloat(oColumn.getWidth())
+                };
+            });
+            new Spreadsheet({
+                dataSource: aSelectedData, // 仅传递选中的数据
+                columns: aColumns,
+                fileName: "Export.xlsx",
+                worker: false,  // Disable web worker for simplicity
+                beforeExport: this.onBeforeExport.bind(this)
+            }).build().finally(function () {
+                sap.m.MessageToast.show(this._ResourceBundle.getText("exportFinished"));
+            }.bind(this));
+        },
+
+        onBeforeExport: function (oEvent) {
+            var oTable = this.getView().byId("detailTable");
+            var aSelectedIndices = oTable.getSelectedIndices();
+
+            if (aSelectedIndices.length === 0) {
+                sap.m.MessageToast.show(this._ResourceBundle.getText("選択されたデータがありません、データを選択してください。")); // 提示未选择数据
+                oEvent.preventDefault(); // 取消导出操作
+                return;
+            }
+            // var mExcelSettings = oEvt.getParameter("exportSettings");
+            // for (var i = 0; i < mExcelSettings.workbook.columns.length; i++) {
+            //     // this.CommTools._setExcelFormatDate(mExcelSettings, i, "VALID_DATE_FROM");
+            //     // this.CommTools._setExcelFormatDate(mExcelSettings, i, "VALID_DATE_TO");
+            //     // this.CommTools._setExcelFormatDateTime(mExcelSettings, i, "CD_TIME");
+            //     // this.CommTools._setExcelFormatDateTime(mExcelSettings, i, "UP_TIME");
+            //     // this.CommTools._setExcelFormatDateTime(mExcelSettings, i, "LAST_LOGIN_TIME");
+            //     }
+          
+
+            var oSettings = oEvent.getParameter("exportSettings");
+            if (oSettings) {
+                console.log("onBeforeExport called");
+                console.log("Export Settings:", oSettings);
+                var oDate = new Date();
+                var sDate = oDate.toISOString().slice(0, 10).replace(/-/g, '');
+                var sTime = oDate.toTimeString().slice(0, 8).replace(/:/g, '');
+                // 设置文件名为当前日期和时间
+                oSettings.fileName = `消費税差額差処理_${sDate}${sTime}.xlsx`;
+            }
+        },
+		
+	
+	});
 });
