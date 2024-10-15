b30f285f70ff97592b6faf59b498fd06f1fbf3ca
xy-wang <xiaoyue.wang@sh.shin-china.com>
2024-10-15 19:58:01 +0800

    pch07handler

--- srv/src/main/java/customer/handlers/pch/Pch07Handler.java
+++ srv/src/main/java/customer/handlers/pch/Pch07Handler.java
@@ -5,10 +5,13 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
 import com.sap.cds.services.handler.annotations.On;
 import com.sap.cds.services.handler.annotations.ServiceName;
+import cds.gen.tableservice.PCH07CheckDATAContext;
+import cds.gen.tableservice.PCH07SaveDATAContext;
 
 import cds.gen.tableservice.TableService_;
 import customer.bean.pch.Pch07DataList;
 import customer.service.pch.Pch07Service;
+
 
 import com.alibaba.fastjson.JSON;
 import com.sap.cds.services.handler.EventHandler;
@@ -22,22 +25,20 @@ public class Pch07Handler implements EventHandler {
     @Autowired
     private Pch07Service Pch07Service;
 
-    
-  
-  //   // check数据
-  //   @On(event = "PCH07_CHECK_DATA")
-  //   public void checkData(PCH07CheckDATAContext context) {
-  //       Pch07DataList list = JSON.parseObject(context.getShelfJson(), Pch07DataList.class);
-  //   Pch07Service.detailsCheck(list);
-  //   context.setResult(JSON.toJSONString(list));
-  //   }
+    // check数据
+    @On(event = "PCH07_CHECK_DATA")
+    public void checkData(PCH07CheckDATAContext context) {
+        Pch07DataList list = JSON.parseObject(context.getShelfJson(), Pch07DataList.class);
+    Pch07Service.detailsCheck(list);
+    context.setResult(JSON.toJSONString(list));
+    }
 
-  //     // 保存数据
-  // @On(event = "PCH07_SAVE_DATA")
-  //   public void saveData(PCH07SaveDATAContext context) {
-  //       Pch07DataList list = JSON.parseObject(context.getShelfJson(), Pch07DataList.class);
-  //   // Pch07Service.detailsSave(list);
-  //   context.setResult(JSON.toJSONString(list));
-  // }
+      // 保存数据
+  @On(event = "PCH07_SAVE_DATA")
+    public void saveData(PCH07SaveDATAContext context) {
+        Pch07DataList list = JSON.parseObject(context.getShelfJson(), Pch07DataList.class);
+    // Pch07Service.detailsSave(list);
+    context.setResult(JSON.toJSONString(list));
+  }
 
 }
