2b4b9cbdb24a58ff58690123d56ca7149ca33492
liangxiaorui <liangxiaorui@outlook.com>
2024-10-20 12:20:02 +0800

    Update PCH08 List View

--- app/webapp/controller/pch/pch08_main.controller.js
+++ app/webapp/controller/pch/pch08_main.controller.js
@@ -6,7 +6,7 @@ sap.ui.define([
     return Controller.extend("umc.app.controller.pch.pch08_main", {
         onInit: function () {
             // 加载默认的详细视图
-            const defaultView = sap.ui.xmlview("umc.app.view.pch.pch05_account_d");
+            const defaultView = sap.ui.xmlview("umc.app.view.pch.pch08_list");
             this.getView().byId("idIconTabBar").addContent(defaultView);
         },
 
