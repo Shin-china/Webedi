b8ab776a1dafdb249d2abd67a5eb6fc082d3aa56
Nero.w <zhao.wang@sh.shin-china.com>
2024-10-31 13:49:33 +0800

    job04 add

---
+++ srv/src/main/java/customer/bean/pch/Items.java
@@ -0,0 +1,24 @@
+package customer.bean.pch;
+
+import lombok.Data;
+
+@Data
+public class Items {
+
+    private String PrNumber; // 采购请求编号
+    private String DNo; // 文档编号
+    private String PurGroup; // 采购组
+    private String Supplier; // 供应商
+    private String Material; // 物料
+    private String MaterialText; // 物料文本
+    private String DelivaryDays; // 交货天数
+    private String ArrangeStartDate; // 预约开始日期
+    private String ArrangeEndDate; // 预约结束日期
+    private String Plant; // 工厂
+    private String ArrangeQty; // 预约数量
+    private String Name1; // 名称1
+    private String MinDeliveryQty; // 最小交货数量
+    private String ManufCode; // 生产商代码
+    private String PurGroupName; // 采购组名称
+
+}
---
+++ srv/src/main/java/customer/service/ifm/Ifm04PrService.java
@@ -0,0 +1,84 @@
+package customer.service.ifm;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.alibaba.fastjson.JSON;
+import java.time.LocalDate;
+import java.math.BigDecimal;
+import java.time.format.DateTimeFormatter;
+
+import cds.gen.pch.T09Forcast;
+import cds.gen.sys.T11IfManager;
+import customer.bean.pch.Items;
+import customer.bean.pch.SapPrRoot;
+import customer.dao.pch.PurchaseDataDao;
+import customer.dao.sys.IFSManageDao;
+import customer.odata.S4OdataTools;
+
+@Component
+public class Ifm04PrService {
+
+    @Autowired
+    private IFSManageDao ifsManageDao;
+
+    @Autowired
+    private PurchaseDataDao PchDao;
+
+    public String syncPr() {
+
+        try {
+
+            // 获取 Web Service 配置信息
+            T11IfManager webServiceConfig = ifsManageDao.getByCode("IFM65");
+
+            if (webServiceConfig != null) {
+
+                String response = S4OdataTools.get(webServiceConfig, 1000, null, null);
+
+                SapPrRoot sapPrRoot = JSON.parseObject(response, SapPrRoot.class);
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+                for (Items item : sapPrRoot.getItems()) {
+
+                    // 处理单个 PR 数据
+                    T09Forcast o = T09Forcast.create();
+                    o.setPrNumber(item.getPrNumber());
+                    o.setDNo(Integer.parseInt(item.getDNo())); // 假设 item.getDNo() 返回的是 String 类型
+                    o.setPurGroup(item.getPurGroup());
+                    o.setSupplier(item.getSupplier());
+                    o.setMaterial(item.getMaterial());
+                    o.setMaterialText(item.getMaterialText());
+                    o.setDelivaryDays(Integer.parseInt(item.getDelivaryDays())); // 假设 item.getDelivaryDays() 返回的是
+                                                                                 // String 类型
+                    o.setArrangeStartDate(LocalDate.parse(item.getArrangeStartDate(), formatter)); // 假设
+                                                                                                   // item.getArrangeStartDate()
+                                                                                                   // // 为字符串
+                    o.setArrangeEndDate(LocalDate.parse(item.getArrangeEndDate())); // 假设 item.getArrangeEndDate() 为字符串
+                    o.setPlant(item.getPlant());
+                    o.setArrangeQty(new BigDecimal(item.getArrangeQty())); // 假设 item.getArrangeQty() 返回的是 String 类型
+                    o.setName1(item.getName1());
+                    o.setMinDeliveryQty(new BigDecimal(item.getMinDeliveryQty())); // 假设 item.getMinDeliveryQty() 返回
+                                                                                   // String 类型
+                    o.setManufCode(item.getManufCode());
+                    o.setPurGroupName(item.getPurGroupName());
+
+                    PchDao.modify3(o);
+
+                }
+
+                return "success";
+            } else {
+
+                return "error";
+            }
+
+        } catch (Exception e) {
+
+            return e.getMessage();
+
+        }
+
+    }
+
+}
