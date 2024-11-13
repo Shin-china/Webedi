33a9c547474224c0b7bc7314794d2c603d0612c0
Nero.w <zhao.wang@sh.shin-china.com>
2024-11-13 20:20:49 +0800

    job change

--- srv/src/main/java/customer/dao/pch/PurchaseDataDao.java
+++ srv/src/main/java/customer/dao/pch/PurchaseDataDao.java
@@ -3,6 +3,7 @@ package customer.dao.pch;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Optional;
 
 import org.checkerframework.checker.units.qual.t;
@@ -146,13 +147,13 @@ public class PurchaseDataDao extends Dao {
         db.run(Update.entity(Pch_.T09_FORCAST).entry(o));
     }
 
-    public Boolean getByPoDnUpdateOrInsertObj(Item items) {
+    public Boolean getByPoDnUpdateObj(Item items) {
         Integer dn = Integer.parseInt(items.getPurchaseorderitem());
 
         T02PoD isExist = getByID2(items.getPurchaseorder(), dn);
         if (isExist == null) {
 
-            return true;
+            return false;
 
         } else {
 
@@ -177,13 +178,48 @@ public class PurchaseDataDao extends Dao {
                 || !isExist.getPoPurQty().equals(PoPurQty)
                 || isExist.getPoDDate().isEqual(deliveryDate)
                 || isExist.getDelAmount().equals(amount)
-                || isExist.getDelFlag() != items.getPurchasingdocumentdeletioncode()
+                //删除flag，单独考虑
+                // || isExist.getDelFlag() != items.getPurchasingdocumentdeletioncode()
 
         ) {
             return true; // 只要有一个值不一样，返回 true
         }
 
         return false;
+
+    }
+
+
+
+    public Boolean getDelflaghavechange(Item items) {
+
+        return false;
+
+    }
+
+    public Boolean getdelbyPO(String purchaseorder) {
+
+        List<T02PoD> result = getByPo(purchaseorder);
+
+        for (T02PoD t02PoD : result) {
+            // 如果当前po下，任意一个明细的delflag为N，则返回false
+            if ("N".equals(t02PoD.getDelFlag())) {
+                return false; // 遇到"N"即返回false
+            }
+        }
+
+        // 如果所有的delflag都为Y，则返回true
+        return true; // 只有当没有"N"时，返回true
+    }
+    
+    public List<T02PoD> getByPo(String po) {
+
+        List<T02PoD> listOf = db.run(
+                Select.from(Pch_.T02_PO_D)
+                        .where(o -> o.PO_NO().eq(po)))
+                .listOf(T02PoD.class);
+
+        return listOf;
 
     }
 
--- srv/src/main/java/customer/service/ifm/Ifm03PoService.java
+++ srv/src/main/java/customer/service/ifm/Ifm03PoService.java
@@ -75,6 +75,10 @@ public class Ifm03PoService {
 
                 Map<String, String> supplierMap = new HashMap<>();
 
+                Map<String, String> supplierCreatMap = new HashMap<>();
+                Map<String, String> supplierUpdateMap = new HashMap<>();
+                Map<String, String> supplierDeleteMap = new HashMap<>();
+
                 Collection<MailJson> mailJsonList = new ArrayList<>();
 
                 Integer key = 0;
@@ -83,131 +87,242 @@ public class Ifm03PoService {
 
                 for (Item Items : sapPchRoot.getItems()) {
 
-                    isMailObj = PchDao.getByPoDnUpdateOrInsertObj(Items);
+                    int dn = Integer.parseInt(Items.getPurchaseorderitem());
 
-                    T01PoH o = T01PoH.create();
-                    o.setPoNo(Items.getPurchaseorder());
-                    o.setPoBukrs(Items.getCompanycode());
+                    if ("X".equals(Items.getPurchasingdocumentdeletioncode())
+                            && PchDao.getByID2(Items.getPurchaseorder(), dn) == null) {
 
-                    try {
+                        // 当前02表中没有本条，传进来的是删除标记还是x ，则不进行插入操作
 
-                        LocalDate poDate = LocalDate.parse(Items.getPurchaseorderdate(), formatter); // 转换字符串为
-                                                                                                     // LocalDate
-                        o.setPoDate(poDate);
+                    } else {
+                        T01PoH o = T01PoH.create();
 
-                    } catch (DateTimeParseException e) {
+                        T01PoH T01have = PchDao.getByID(Items.getPurchaseorder());
+                        String supplier = Items.getSupplier().replaceFirst("^0+(?!$)", "");
 
-                        System.out.println("日期格式无效: " + e.getMessage()); // 处理格式错误
+                        // 如果是空的，则插入，更新supplier creat名单
+                        if (T01have == null) {
 
-                    }
-                    // o.setSupplier(Items.getSupplier());
-                    // 去除前导 0
-
-                    String supplier = Items.getSupplier().replaceFirst("^0+(?!$)", "");
-
-                    o.setSupplier(supplier);
-
-                    PchDao.modify(o);
-
-                    T02PoD o2 = T02PoD.create();
-                    o2.setPoNo(Items.getPurchaseorder());
-
-                    int number = Integer.parseInt(Items.getPurchaseorderitem());
-                    o2.setDNo(number);
-
-                    o2.setMatId(Items.getMaterial());
-
-                    o2.setPlantId(Items.getPlant());
-                    o2.setPoDTxz01(Items.getPurchaseorderitemtext());
-                    Items.getOrderquantity();
-
-                    o2.setPoPurQty(new BigDecimal(Items.getOrderquantity()));
-                    o2.setPoPurUnit(Items.getPurchaseorderquantityunit());
-
-                    o2.setCurrency(Items.getDocumentcurrency());
-
-                    try {
-
-                        LocalDate deliveryDate = LocalDate.parse(Items.getSchedulelinedeliverydate(), formatter);
-                        o2.setPoDDate(deliveryDate);
-
-                    } catch (DateTimeParseException e) {
-                        e.printStackTrace();
-
-                    }
-
-                    try {
-                        // 将字符串转换为 BigDecimal
-                        BigDecimal netpriceAmount = new BigDecimal(Items.getNetpriceamount());
-                        BigDecimal netPriceQuantity = new BigDecimal(Items.getNetpricequantity());
-
-                        // 检查 netPriceQuantity 是否为 0，以避免除以 0 的情况
-                        if (netPriceQuantity.compareTo(BigDecimal.ZERO) != 0) {
-                            // 计算并设置价格，使用指定为具有两位小数的舍入模式
-                            BigDecimal delPrice = netpriceAmount.divide(netPriceQuantity, 2, RoundingMode.HALF_UP);
-                            o2.setDelPrice(delPrice);
-                        } else {
-                            // 处理除以 0 的情况，例如设置为 0 或抛出异常
-                            o2.setDelPrice(BigDecimal.ZERO); // 或其他逻辑
-                        }
-                    } catch (NumberFormatException e) {
-                        e.printStackTrace(); // 处理转换错误
-                    } catch (Exception e) {
-                        e.printStackTrace(); // 处理其他可能的异常
-                    }
-
-                    o2.setDelAmount(new BigDecimal(Items.getNetamount()));
-                    o2.setUnitPrice(new BigDecimal(Items.getNetpricequantity()));
-                    o2.setStorageLoc(Items.getStoragelocation());
-                    o2.setStorageTxt(Items.getStoragelocationname());
-                    o2.setMemo(Items.getPlainlongtext());
-
-                    o2.setSupplierMat(Items.getSupplierMaterialNumber());
-
-                    if (Items.getPurchasingdocumentdeletioncode() == "X") {
-
-                        dele = true;
-                    }
-
-                    PchDao.modify2(o2, dele);
-
-                    // 记录供应商信息，供后续邮件发送使用
-                    // 判断是否需要更新或插入对象 (購買伝票 + 明細番号 + 発注数量 + 納入日付 + 発注単価 + 削除フラグ+MRP減少数量 有变化时才是变更对象
-                    // podn 找不到则也是变更对象)
-                    if (isMailObj) {
-
-                        key++;
-                        if (!supplierMap.containsKey(supplier)) {
-
-                            supplierMap.put(supplier, supplier);
+                            supplierCreatMap.put(supplier, "头没找到，肯定是创建");
 
                         }
-                        ;
 
+                        // 记录供应商信息，供后续邮件发送使用
+                        // 判断是否需要更新或插入对象 (購買伝票 + 明細番号 + 発注数量 + 納入日付 + 発注単価 + 削除フラグ+MRP減少数量 有变化时才是变更对象
+                        // podn 找不到则也是变更对象)
+                        Boolean isUpdateObj = PchDao.getByPoDnUpdateObj(Items);
+
+                        if (isUpdateObj) {
+
+                            if (!supplierUpdateMap.containsKey(supplier)) {
+
+                                supplierUpdateMap.put(supplier, "四个字段其中有修改，肯定是更新");// delflag 单独考虑。
+
+                            }
+                            ;
+                        }
+
+                        // 判断是否需要删除对象 (删除标记为X)
+                        // 前提条件：
+                        // 1.有对删除标记的修改
+
+                        // 两种情况
+                        // 1.当前po下的所有明细都有删除标记
+                        // 2.当前po下的所有明细不是都有删除标记
+                        Boolean isDelflaghavechange = PchDao.getDelflaghavechange(Items);
+
+                        o.setPoNo(Items.getPurchaseorder());
+                        o.setPoBukrs(Items.getCompanycode());
+
+                        try {
+
+                            LocalDate poDate = LocalDate.parse(Items.getPurchaseorderdate(), formatter); // 转换字符串为
+                                                                                                         // LocalDate
+                            o.setPoDate(poDate);
+
+                        } catch (DateTimeParseException e) {
+
+                            System.out.println("日期格式无效: " + e.getMessage()); // 处理格式错误
+
+                        }
+
+                        // 去除前导 0 后的
+                        o.setSupplier(supplier);
+
+                        PchDao.modify(o);
+                        // ------------------------------------------------------------------------------------以上是对t01的修改
+
+                        T02PoD o2 = T02PoD.create();
+                        o2.setPoNo(Items.getPurchaseorder());
+
+                        o2.setDNo(dn);
+
+                        o2.setMatId(Items.getMaterial());
+
+                        o2.setPlantId(Items.getPlant());
+                        o2.setPoDTxz01(Items.getPurchaseorderitemtext());
+                        Items.getOrderquantity();
+
+                        o2.setPoPurQty(new BigDecimal(Items.getOrderquantity()));
+                        o2.setPoPurUnit(Items.getPurchaseorderquantityunit());
+
+                        o2.setCurrency(Items.getDocumentcurrency());
+
+                        try {
+
+                            LocalDate deliveryDate = LocalDate.parse(Items.getSchedulelinedeliverydate(), formatter);
+                            o2.setPoDDate(deliveryDate);
+
+                        } catch (DateTimeParseException e) {
+                            e.printStackTrace();
+
+                        }
+
+                        try {
+                            // 将字符串转换为 BigDecimal
+                            BigDecimal netpriceAmount = new BigDecimal(Items.getNetpriceamount());
+                            BigDecimal netPriceQuantity = new BigDecimal(Items.getNetpricequantity());
+
+                            // 检查 netPriceQuantity 是否为 0，以避免除以 0 的情况
+                            if (netPriceQuantity.compareTo(BigDecimal.ZERO) != 0) {
+                                // 计算并设置价格，使用指定为具有两位小数的舍入模式
+                                BigDecimal delPrice = netpriceAmount.divide(netPriceQuantity, 2, RoundingMode.HALF_UP);
+                                o2.setDelPrice(delPrice);
+                            } else {
+                                // 处理除以 0 的情况，例如设置为 0 或抛出异常
+                                o2.setDelPrice(BigDecimal.ZERO); // 或其他逻辑
+                            }
+                        } catch (NumberFormatException e) {
+                            e.printStackTrace(); // 处理转换错误
+                        } catch (Exception e) {
+                            e.printStackTrace(); // 处理其他可能的异常
+                        }
+
+                        o2.setDelAmount(new BigDecimal(Items.getNetamount()));
+                        o2.setUnitPrice(new BigDecimal(Items.getNetpricequantity()));
+                        o2.setStorageLoc(Items.getStoragelocation());
+                        o2.setStorageTxt(Items.getStoragelocationname());
+                        o2.setMemo(Items.getPlainlongtext());
+
+                        o2.setSupplierMat(Items.getSupplierMaterialNumber());
+
+                        if (Items.getPurchasingdocumentdeletioncode() == "X") {
+
+                            dele = true;
+                        }
+
+                        //
+                        PchDao.modify2(o2, dele);
+
+                        // ------------------------------------------------------------------------------以上是对t02的修改
+
+                        // 首先是，dele被修改的情况下再继续。
+                        if (isDelflaghavechange) {
+                            Boolean isalldele = PchDao.getdelbyPO(Items.getPurchaseorder());
+
+                            if (isalldele) {
+                                // 全部明细都有删除标记
+                                supplierDeleteMap.put(supplier, "全部明细都有删除标记，肯定是删除");
+
+                            }
+                        }
+                    }
+                }
+
+                // // 处理邮件通知
+                // if (supplierMap.size() > 0) {
+
+                // for (Map.Entry<String, String> entry : supplierMap.entrySet()) {
+
+                // MailJson mailJson = MailJson.create();
+
+                // List<T08ComOpD> emailadd = sysd008dao.getmailaddByHcodeV1(H_CODE,
+                // entry.getValue());
+
+                // if (emailadd != null) {
+
+                // mailJson.setTemplateId("UWEB_M004_C");
+
+                // mailJson.setMailTo(emailadd.get(0).getValue02());
+
+                // mailJson.setMailBody(createMailBody(emailadd)); // 设置邮件内容（MailBody）
+
+                // // 添加到邮件列表
+                // mailJsonList.add(mailJson);
+
+                // // 调用邮件发送服务
+                // try {
+                // emailServiceFun.sendEmailFun(mailJsonList);
+                // // 设置操作结果
+                // return ("メール送信に成功しました。");
+                // } catch (Exception e) {
+                // // 处理发送邮件的异常
+                // return e.getMessage();
+                // }
+                // }
+                // }
+
+                // }
+                // 创建发信
+                if (supplierCreatMap.size() > 0) {
+                    for (Map.Entry<String, String> entry : supplierCreatMap.entrySet()) {
+
+                        MailJson mailJson = MailJson.create();
+                        List<T08ComOpD> emailadd = sysd008dao.getmailaddByHcodeV1(H_CODE, entry.getValue());
+                        if (emailadd != null) {
+                            mailJson.setTemplateId("UWEB_M004_C");
+                            mailJson.setMailTo(emailadd.get(0).getValue02());
+                            mailJson.setMailBody(createMailBody(emailadd)); // 设置邮件内容（MailBody）
+                            // 添加到邮件列表
+                            mailJsonList.add(mailJson);
+                            // 调用邮件发送服务
+                            try {
+                                emailServiceFun.sendEmailFun(mailJsonList);
+                                // 设置操作结果
+                                return ("メール送信に成功しました。");
+                            } catch (Exception e) {
+                                // 处理发送邮件的异常
+                                return e.getMessage();
+                            }
+                        }
                     }
 
                 }
-
-                // 处理邮件通知
-                if (supplierMap.size() > 0) {
-
-                    for (Map.Entry<String, String> entry : supplierMap.entrySet()) {
-
+                // 更新发信
+                if (supplierUpdateMap.size() > 0) {
+                    for (Map.Entry<String, String> entry : supplierUpdateMap.entrySet()) {
                         MailJson mailJson = MailJson.create();
-
                         List<T08ComOpD> emailadd = sysd008dao.getmailaddByHcodeV1(H_CODE, entry.getValue());
-
                         if (emailadd != null) {
-
-                            mailJson.setTemplateId("UWEB_M004_C");
-
+                            mailJson.setTemplateId("UWEB_M004_U");
                             mailJson.setMailTo(emailadd.get(0).getValue02());
-
                             mailJson.setMailBody(createMailBody(emailadd)); // 设置邮件内容（MailBody）
-
                             // 添加到邮件列表
                             mailJsonList.add(mailJson);
+                            // 调用邮件发送服务
+                            try {
+                                emailServiceFun.sendEmailFun(mailJsonList);
+                                // 设置操作结果
+                                return ("メール送信に成功しました。");
+                            } catch (Exception e) {
+                                // 处理发送邮件的异常
+                                return e.getMessage();
+                            }
+                        }
+                    }
 
+                }
+                // 删除发信
+                if (supplierDeleteMap.size() > 0) {
+                    for (Map.Entry<String, String> entry : supplierDeleteMap.entrySet()) {
+                        MailJson mailJson = MailJson.create();
+                        List<T08ComOpD> emailadd = sysd008dao.getmailaddByHcodeV1(H_CODE, entry.getValue());
+                        if (emailadd != null) {
+                            mailJson.setTemplateId("UWEB_M004_D");
+                            mailJson.setMailTo(emailadd.get(0).getValue02());
+                            mailJson.setMailBody(createMailBody(emailadd)); // 设置邮件内容（MailBody）
+                            // 添加到邮件列表
+                            mailJsonList.add(mailJson);
                             // 调用邮件发送服务
                             try {
                                 emailServiceFun.sendEmailFun(mailJsonList);
