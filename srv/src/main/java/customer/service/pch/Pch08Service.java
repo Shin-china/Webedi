d6d594754d8ad245ca936c02950577be7f4c26cb
liangxiaorui <liangxiaorui@outlook.com>
2024-11-19 10:56:06 +0800

    MM-035 Add new records to T07 table when price changed

--- srv/src/main/java/customer/service/pch/Pch08Service.java
+++ srv/src/main/java/customer/service/pch/Pch08Service.java
@@ -322,7 +322,9 @@ public class Pch08Service {
                 T07QuotationD t07New = d007Dao.getByT07Id(t07Id);
 
                 if(t07 != null) {
-                    t07.setDelFlag("N");
+                    t07.setDelFlag("Y");
+                    t07.setUpTime(DateTools.getInstantNow());
+                    t07.setUpBy(pch08Dao.getUserId());
                     d007Dao.update(t07);
                 }
 
@@ -331,6 +333,7 @@ public class Pch08Service {
                     t07New.setPrice(price == null ? BigDecimal.ZERO : new BigDecimal(price));
                     t07New.setId(null);
                     t07New.setStatus("3");
+                    t07New.setDelFlag("N");
                     d007Dao.insert(t07New);
                 }
             }
