40d9c997254edcecdbe582860b66e699fc901099
xy-wang <xiaoyue.wang@sh.shin-china.com>
2024-11-25 16:48:10 +0800

    INV_CONFIRMATION更改

--- app/webapp/controller/pch/pch05_account_d.controller.js
+++ app/webapp/controller/pch/pch05_account_d.controller.js
@@ -320,7 +320,7 @@ sap.ui.define([
                 return false;
             }
 
-            var invdate = new Set(aSelectedData.map(data => data.INV_CONFIRMATION);
+            var invdate = new Set(aSelectedData.map(data => data.INV_CONFIRMATION));
             // 检查是否没有 "確認" 的记录
             if (!invdate.has("確定")) {
                 sap.m.MessageBox.error("「インボイス確定」処理未実施の時は、[Send Mail」及び、「Print」を不可とする");
