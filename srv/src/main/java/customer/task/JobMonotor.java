0a30183e98634e247590de847232c69472745c9b
Nero.w <zhao.wang@sh.shin-china.com>
2024-12-19 10:00:33 +0800

    和客户合意之后的执行时间

--- srv/src/main/java/customer/task/JobMonotor.java
+++ srv/src/main/java/customer/task/JobMonotor.java
@@ -48,21 +48,21 @@ public class JobMonotor {
     private Ifm06BpPurchaseService ifm06BpPurchaseService;
 
     // 改造 定时任务
-    @Scheduled(cron = "0 1 13 * * *") // 中国时间21:01点执行
+    @Scheduled(cron = "0 1 21 * * *") // 日本时间早晨6：01执行
     public void poolMonitor() throws IOException {
         // BP 同期 标准api
         IFLog ifLog = new IFLog(IFSManageDao.IF_S4_BP);
         ifm01BpService.process(ifLog);
     }
 
-    @Scheduled(cron = "0 2 13 * * *") // 中国时间21:02点执行
+    @Scheduled(cron = "0 2 21 * * *") // 日本时间早晨6：02执行
     public void poolMonitor6() throws IOException {
         // BP PURCHASE 标准api
         IFLog ifLog = new IFLog(IFSManageDao.IF_S4_BPPURCHASE);
         ifm06BpPurchaseService.process(ifLog);
     }
 
-    @Scheduled(cron = "0 0/15 * * * ?") // 每15分钟执行一次
+    @Scheduled(cron = "0 0/30 * * * ?") // 每30分钟执行一次
     public void poolMonitor1() throws IOException {
         // IF039 品目同期 标准api
         IFLog ifLog = new IFLog(IFSManageDao.IF_S4_MST);
@@ -70,7 +70,7 @@ public class JobMonotor {
 
     }
 
-    @Scheduled(cron = "0 0/15 * * * ?")
+    @Scheduled(cron = "0 0/30 * * * ?") // 每30分钟执行一次
     public void poolMonitor3() throws IOException {
 
         // IF041 po同期 自开发
@@ -79,7 +79,7 @@ public class JobMonotor {
 
     }
 
-    @Scheduled(cron = "0 0/15 * * * ?")
+    @Scheduled(cron = "0 3 21 * * *") // 日本时间早晨6：03执行
     public void poolMonitor4() throws IOException {
 
         // IF065 フォーキャスト連携 自开发
@@ -88,7 +88,7 @@ public class JobMonotor {
 
     }
 
-    @Scheduled(cron = "0 0/15 * * * ?")
+    @Scheduled(cron = "0 4 21 * * *") // 日本时间早晨6：04执行
     public void poolMonitor5() throws IOException {
 
         // IF042 PO支払通知同期 自开发
